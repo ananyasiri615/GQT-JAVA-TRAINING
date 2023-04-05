b31296e5ac7268f497fd93175bef7a9da5c8133c
Ananya R Siri <ananyasiri615@gmail.com>
2023-04-05 19:13:37 +0530

    1

--- src/gqtcodes/Program1.java
+++ src/gqtcodes/Program1.java
@@ -1,10 +1,37 @@
 package gqtcodes;
+import java.util.Scanner;
 
 public class Program1 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the count of university: ");
+		int uni =  sc.nextInt();
+		System.out.println("Enter the count of Colleges: ");
+		int scl =  sc.nextInt();
+		System.out.println("Enter the count of classes: ");
+		int cls =  sc.nextInt();
+		System.out.println("Enter the count of students: "); 
+		int stu =  sc.nextInt();
+		int arr[][][] = new int[scl] [cls] [stu]; // three dimensional array
+		// storing the marks
+		for (int i = 0; i < scl; i++) {
+			System.out.println("Inside school :"+ (i + 1)); 
+				for (int j = 0; j < cls; j++) {
+					System.out.println("Inside class:" + (j + 1)); 
+					for (int k = 0; k < stu; k++) {
+						System.out.println("Enter marks of student no:" +(k + 1));
+						arr[i][j] [k] = sc.nextInt();
+					}
+				}	
+			}
+			for (int i = 0 ; i < scl; i++) {
+				System.out.println("Inside school :" + (i + 1) ) ; 
+				for (int j=0; j < cls; j++) {
+					System.out.println("Inside class :"+ (j + 1) ) ; 
+					for (int k = 0; k < stu; k++) {
+							System.out.println("The marks of student no: " +(k+1)+" is: " + arr[i][j] [k]);
+			}
+				}}
 	}
-
 }
