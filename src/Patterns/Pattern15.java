77705dabb196b82b73b3052045d6b39ecf9f884b
Ananya R Siri <ananyasiri615@gmail.com>
2023-04-12 18:48:46 +0530

    Patterns2

--- src/Patterns/Pattern15.java
+++ src/Patterns/Pattern15.java
@@ -1,18 +1,18 @@
 package Patterns;
 import java.util.Scanner;
 
-public class Pattern15{
+public class Pattern15 {
+
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the value:");
+		Scanner sc  = new Scanner(System.in);
+		System.out.println("Enter the value of n ");
 		int n = sc.nextInt();
-		for(int i=1;i<=n;i++) {
-			for(int j=n;j>=i;j--) { //*
-				System.out.print("* ");
+		for(int i = 1; i<=n; i++){
+			for(int j = n; j>= i; j--) {
+				System.out.print("& ");
 			}
 			System.out.println();
 		}
-		
 	}
 }
 
@@ -58,3 +58,12 @@ public class Pattern15{
  * 
  * 		j>=i ---> 1>=2 (condition false)
  */
+
+/*
+ * 		* * * * *
+ * 		* * * *
+ * 		* * *
+ * 		* *
+ * 		*
+ */
+*/
---
+++ src/Patterns/Pattern20.java
@@ -0,0 +1,34 @@
+package Patterns;
+import java.util.Scanner;
+
+public class Pattern20 {
+
+	public static void main(String[] args) {
+		Scanner sc  = new Scanner(System.in);
+		System.out.println("Enter the value of n");
+		int n = sc.nextInt();
+		for(int i = 1; i<=n; i++){
+			for(int j = n; j>= i; j--) {
+				System.out.print("& ");
+			}
+			for(int j=1; j<=n; j++) {
+				System.out.print("# ");
+
+			}
+			System.out.println();
+		}
+
+	}
+
+}
+
+/*
+ * Enter the value of n
+5
+& & & & & # # # # # 
+& & & & # # # # # 
+& & & # # # # # 
+& & # # # # # 
+& # # # # # 
+
+ */
